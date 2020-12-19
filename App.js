2fc1f5166486cbab5efbfcca682a7db95bf6b489
khNishad <kkhnishad@gmail.com>
2020-12-19 18:45:35 +0600

    webView

--- App.js
+++ App.js
@@ -24,7 +24,6 @@ const App = () => {
    
     if (webviewRef.current){
         webviewRef.current.goBack()
-       
     }
     
    }
@@ -53,11 +52,15 @@ const App = () => {
     <>
       <StatusBar barStyle='light-content' />
       <SafeAreaView style={styles.flexContainer} >
-         {/* <View  style = {{justifyContent: 'center',alignItems:'center',backgroundColor:'#b43757'}}>
-              <Image source = {{uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png'}}
-              style = {{ width: 100, height: 100}}
-              />
-         </View> */}
+
+         <View  style = {{justifyContent: 'center',alignItems:'center', backgroundColor: '#F0F2F5' }}>
+         <Image
+        
+          source={require("./assets/seller-app-icon.png")}
+          style={{ width: 80, height: 100}}
+        />
+         </View>
+       
           <WebView
             source={{ uri: 'https://seller.ebhubon.com' }}
             startInLoadingState={true}
